b6e1610b35af82167fa9a897888676055fbb3ec4
tobiasg <tobiasg@users.noreply.github.com>
2025-09-18 16:11:05 +0200

    Extract form to a new component

--- src/features/auth/components/Input.tsx
+++ src/features/auth/components/Input.tsx
@@ -1,13 +1,14 @@
-import { type ChangeEvent, type ReactElement, type RefObject } from "react";
+import { type ChangeEvent, type ReactElement } from "react";
 
 interface InputProps {
   label: string;
   name: string;
-  type: "text" | "email" | "password" | "submit";
+  type: "text" | "email" | "password";
   value: string;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
   autoFocus: boolean;
   disabled: boolean;
+  autoComplete?: string;
 }
 
 export const Input = ({
@@ -18,29 +19,26 @@ export const Input = ({
   onChange,
   autoFocus,
   disabled,
+  autoComplete,
 }: InputProps): ReactElement => {
   const id = `${name.toLowerCase().replace(/\s+/g, "-")}`;
 
-  if (type === "submit") {
-    return <input id={id} type={type} value={label} disabled={disabled} />;
-  } else {
-    return (
-      <>
-        <label>
-          {label}
-          <input
-            id={id}
-            type={type}
-            name={name}
-            value={value}
-            onChange={onChange}
-            autoComplete="on"
-            required
-            autoFocus={autoFocus}
-            disabled={disabled}
-          />
-        </label>
-      </>
-    );
-  }
+  return (
+    <>
+      <label>
+        {label}
+        <input
+          id={id}
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          autoComplete={autoComplete}
+          required
+          autoFocus={autoFocus}
+          disabled={disabled}
+        />
+      </label>
+    </>
+  );
 };
--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -1,73 +1,22 @@
-import { FormEventHandler, ReactElement, useState } from "react";
+import { ReactElement } from "react";
 import { useNavigate, useSearchParams } from "react-router";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { Input } from "./Input";
-import { CustomError } from "../../shared/classes";
+import { LoginForm } from "./LoginForm";
 
 export function Login(): ReactElement {
-  const [error, setError] = useState<string | null>(null);
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const [searchParams] = useSearchParams();
   const { login } = useAuthContext();
   const navigate = useNavigate();
 
-  const isFormValid = username.trim() !== "" && password.trim() !== "";
-
-  const handleOnSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-
-    try {
-      await login(username, password);
-
-      const redirectTo = searchParams.get("redirectTo") || "/";
-      navigate(redirectTo, { replace: true });
-    } catch (err: unknown) {
-      if (err instanceof CustomError) {
-        setError("Fel e-postadress eller lösenord");
-      } else {
-        setError("Något gick fel. Försök igen");
-      }
-    } finally {
-    }
+  const handleOnSubmit = async (username: string, password: string) => {
+    await login(username, password);
+    const redirectTo = searchParams.get("redirectTo") || "/";
+    navigate(redirectTo, { replace: true });
   };
 
   return (
     <main id="login-page" className="g-container">
-      <div className="login-wrapper">
-        <h1>LMS</h1>
-        <form className="login-form" onSubmit={handleOnSubmit}>
-          <Input
-            label="E-postadress"
-            name="username"
-            type="email"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            autoFocus={true}
-            disabled={false}
-          />
-          <Input
-            label="Lösenord"
-            name="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            autoFocus={false}
-            disabled={false}
-          />
-
-          {error && <div className="error-message">{error}</div>}
-
-          <Input
-            label="Logga in"
-            name=""
-            value=""
-            type="submit"
-            autoFocus={false}
-            disabled={!isFormValid}
-          />
-        </form>
-      </div>
+      <LoginForm onSubmit={handleOnSubmit} />
     </main>
   );
 }
