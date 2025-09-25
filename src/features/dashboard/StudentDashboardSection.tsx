62ade15ce977873deb8aa8aa13a9f4d259dc7e79
ninaeriksson <nina_e74@hotmail.com>
2025-09-25 15:16:36 +0200

    create basic StudentModuleActivities component

--- src/features/dashboard/StudentDashboardSection.tsx
+++ src/features/dashboard/StudentDashboardSection.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StudentCourseCard } from '../shared/components/student-course-card/StudentCourseCard';
+import StudentModuleActivities from '../shared/components/StudentModuleActivities/StudentModuleActivities';
 
 export const StudentDashboardSection: React.FC = () => (
   <div>
     <StudentCourseCard />
+    <StudentModuleActivities />
   </div>
 );
