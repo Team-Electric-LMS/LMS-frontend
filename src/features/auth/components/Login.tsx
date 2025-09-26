bc5ab5ab9a2ab510c271017b7fd2ee4bb2802853
Severin F. <81385477+severinfv@users.noreply.github.com>
2025-09-26 17:26:57 +0200

    Update Login.tsx

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -13,7 +13,7 @@ export function Login(): ReactElement {
   // dashboard component will render different things based on role
   const handleOnSubmit = async (username: string, password: string) => {
     await login(username, password);
-    navigate("/", { replace: true });
+    navigate("/dashboard", { replace: true });
   };
 
   return (
