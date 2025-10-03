bd5327262b2dd0c8ca5ec31640cb07392a4e2a8e
Tomas <tomasantonj@gmail.com>
2025-10-03 11:51:43 +0200

    Move completed, references updated, minor error fix to ICourseLoader typ management

--- src/features/admin/courses/loaders/courseLoader.ts
+++ src/features/admin/courses/loaders/courseLoader.ts
@@ -2,14 +2,15 @@ import { LoaderFunctionArgs } from "react-router";
 import { CustomError } from "../../../shared/classes";
 import { BASE_URL } from "../../../shared/constants";
 import { fetchWithToken } from "../../../shared/utilities";
-import { ICourse, ICourseLoader } from "../../types";
+import { ICourse, ICourseLoader } from "../types";
 
 export const courseLoader = async ({ params }: LoaderFunctionArgs): Promise<ICourseLoader> => {
   if (!params.id) throw new Response("Missing id", { status: 400 });
 
   try {
-    const data = fetchWithToken<ICourse>(`${BASE_URL}/courses/${params.id}`);
-    return { course: data };
+    const data = await fetchWithToken<ICourse>(`${BASE_URL}/courses/${params.id}`);
+    if (!data) throw new Response("Course not found", { status: 404 });
+    return { course: Promise.resolve(data) };
   } catch (e) {
     if (e instanceof CustomError && e.errorCode === 401) {
       throw new Response("Unauthorized", { status: 401 });
--- src/features/courses/components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./CourseCreate";
-export * from "./CourseEdit";
